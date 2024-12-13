8c9a76381635cef551a60bffb4348f3fd413d6ea
MarkFantini <marcelo.asfantini@gmail.com>
2024-12-13 04:15:00 -0300

    Atualização do README

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ import re
 import streamlit as st
 import pandas as pd
 
-# from transform_module.generate_bases import generate_df_dict
-# from transform_module.generate_bases import generate_base_clients, generate_base_lawsuits
-# from transform_module.data_wrangling import clients_data_treatment, lawsuits_data_treatment
+from transform_module.generate_bases import generate_base_clients, generate_base_lawsuits
+from transform_module.data_wrangling import clients_data_treatment, lawsuits_data_treatment
 
 # from load_module.export import write_excel
 
@@ -34,12 +33,21 @@ def fetch_files(csv_files_buffer):
 if csv_files_buffer:
     csv_files, filenames, cod_empresa = fetch_files(csv_files_buffer)
 
+@st.cache_data
+def generate_df_dict(csv_files, filenames):
+    df_dict = dict()
+    latin_encoding = {'v_advogado_adverso', 'v_atendimentos_processo', 'v_clientes'}
+    for csv, name in zip(csv_files, filenames):
+        if name in latin_encoding:
+            df_dict[name] = pd.read_csv(csv, sep=';', encoding='latin1', dtype=str)
+            df_dict[name] = df_dict[name].fillna('')
+        else:
+            df_dict[name] = pd.read_csv(csv, sep=';', encoding='utf-8', dtype=str)
+            df_dict[name] = df_dict[name].fillna('')
 
-for csv in csv_files:
-    try:
-        df = pd.read_csv(csv, sep=';', encoding='latin1', dtype=str)
-        st.write(df.head())
-    except (pd.errors.EmptyDataError, UnicodeDecodeError):
-        df = pd.read_csv(csv, sep=';', encoding='utf-8', dtype=str)
-        st.write(df.head())
+    return df_dict
 
+df_dict = generate_df_dict(csv_files, filenames)
+
+base_clients         = generate_base_clients(df_dict)
+cleaned_base_clients = clients_data_treatment(base_clients, csv_files)
